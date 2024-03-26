4e73377967391dfe889b96bd466f03e5c8b1c17a
Denis <denis.yaremenko@huma.com>
2024-03-26 08:23:37 +0200

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Denis Yaremenko on 26.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
